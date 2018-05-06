11a355c102162921bb2b68339cf951137606a3f5
Wesley <wesley.werner@gmail.com>
2018-05-06 09:17:20 +0200

    Change html theme to classic, add Eureka logo.

--- source/conf.py
+++ source/conf.py
@@ -24,7 +24,7 @@ copyright = u'2018, Eureka Doom Editor'
 author = u'Wesley Werner'
 
 # The short X.Y version
-version = u''
+version = u'1.21'
 # The full version, including alpha/beta/rc tags
 release = u''
 
@@ -34,6 +34,8 @@ release = u''
 # If your documentation needs a minimal Sphinx version, state it here.
 #
 # needs_sphinx = '1.0'
+
+html_logo = 'eureka_logo.png'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -75,13 +77,15 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    "externalrefs": "false"
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
